1aebcb01b8c21a020879c584b58ee8266066aff1
jb6131 <44068903+jb6131@users.noreply.github.com>
2023-11-16 21:29:22 -0600

    added queries to client-side util

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,12 +1,108 @@
 import { gql } from '@apollo/client';
 
+// gets a user by ID
 export const QUERY_USER = gql`
-  {
-    user {
+  query getUser($id: ID!) {
+    user(_id: $id) {
       _id
       firstName
       lastName
       email
+      createdNeeds {
+        _id
+        needText
+        needAuthor {
+          _id
+          firstName
+          lastName
+        }
+        needDate
+        createdAt
+      }
+      signedUpNeeds {
+        _id
+        needText
+        needAuthor {
+          _id
+          firstName
+          lastName
+        }
+        needDate
+        createdAt
+      }
+    }
+  }
+`;
+
+// gets a single need
+export const QUERY_SINGLE_NEED = gql`
+  query getNeed($id: ID!) {
+    need(_id: $id) {
+      _id
+      needText
+      needAuthor {
+        _id
+        firstName
+        lastName
+      }
+      needDate
+      createdAt
+      signedUpUsers {
+        _id
+        firstName
+        lastName
+      }
+    }
+  }
+`;
+
+// gets all needs
+export const QUERY_NEEDS = gql`
+  query getNeeds {
+    allNeeds {
+      _id
+      needText
+      needAuthor {
+        _id
+        firstName
+        lastName
+      }
+      needDate
+      createdAt
+    }
+  }
+`;
+
+// gets currently authenticated user
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      firstName
+      lastName
+      email
+      createdNeeds {
+        _id
+        needText
+        needAuthor {
+          _id
+          firstName
+          lastName
+        }
+        needDate
+        createdAt
+      }
+      signedUpNeeds {
+        _id
+        needText
+        needAuthor {
+          _id
+          firstName
+          lastName
+        }
+        needDate
+        createdAt
+      }
     }
   }
 `;
