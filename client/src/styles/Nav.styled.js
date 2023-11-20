d155595721817d07f758720df58f4ae5e71fb573
Kevin-Stephens <kmstephens437@gmail.com>
2023-11-20 16:29:07 -0600

    converted from Sass to styled component

---
+++ client/src/styles/Nav.styled.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const StyledNav = styled.nav `
+
+padding: 2rem 10%;
+display: flex;
+justify-content:space-around;
+align-items: end;
+box-shadow: 0 0 3px 1px rgba(150, 150, 150, 0.8);
+background-color: green;
+background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKQ127Jcb_5PYbQkwAevptClf9aVP7KZwH-Q&usqp=CAU");
+background-repeat: no-repeat;
+background-size:cover;
+background-position: center center;
+
+p {
+    font-size: 3rem;
+    color: white;
+    display: flex;
+    }
+
+section {
+    background-color: rgb(202, 198, 198);
+    margin: 2rem;
+    font-size: 1.25em;
+    padding: 1rem;
+    border-radius: 1rem;
+
+}
+`
